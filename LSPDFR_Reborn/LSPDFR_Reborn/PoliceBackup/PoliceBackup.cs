f6fc6b5735a8704a0f6a570428bbcfa772904cc9
Adam Nielsen <adam.nielsen@outlook.dk>
2022-12-25 16:47:25 +0100

    Updated RNUI to version 1.9.1

--- LSPDFR_Reborn/LSPDFR_Reborn/PoliceBackup/PoliceBackup.cs
+++ LSPDFR_Reborn/LSPDFR_Reborn/PoliceBackup/PoliceBackup.cs
@@ -1,26 +1,87 @@
 ﻿using LSPDFR_Reborn.Util;
 using Rage;
+using RAGENativeUI;
+using RAGENativeUI.Elements;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LSPDFR_Reborn.PoliceBackup
 {
     internal class PoliceBackup : Process
     {
+        private static Keys s_menuKey = Keys.B; // Only for developing purposes
+
+        private static int s_menuXOffset = 100;
+        private static int s_menuYOffset = 100;
+
+        private static MenuPool _menuPool;
+        private UIMenu _menu;
+
+        private UIMenuListScrollerItem<string> _codeBackup;
+
+        private List<PoliceBackupUnit> _code3Units;
+        private List<PoliceBackupUnit> _code2Units;
+
         public override void Start()
         {
             Game.DisplayNotification("Start PoliceBackup " + DateTime.Now.Millisecond);
+            _menuPool = new MenuPool();
+            _menu = new UIMenu("PoliceBackup", "Call for backup!", System.Drawing.Point.Empty, "phone_wallpaper_blueshards", "phone_wallpaper_blueshards");
+            _menuPool.Add(_menu);
+            _menu.MouseControlsEnabled = false;
+
+            var _filePath = @"C:\Users\adamn\source\repos\Rich-Dunne\LSPDFR-Reborn\LSPDFR_Reborn\LSPDFR_Reborn\PoliceBackup\XMLFile1.xml";
+            var _policeBackupUnits = XMLSerializer.DeserializeXML<PoliceBackupUnits>(_filePath);
+
+            _codeBackup = new UIMenuListScrollerItem<string>("Backup", "", new[] { "Code 3", "Code 2", "K9", "Partner" });
+            _menu.AddItems(_codeBackup);
+
+            _code3Units = new List<PoliceBackupUnit>();
+            _code2Units = new List<PoliceBackupUnit>();
+
+            foreach (PoliceBackupUnit _policeBackupUnit in _policeBackupUnits.PoliceBackupUnit)
+            {
+                try
+                {
+                    if (_policeBackupUnit.Code2.Equals("True"))
+                    {
+                        _code2Units.Add(_policeBackupUnit);
+                    }
+                    if (_policeBackupUnit.Code3.Equals("True"))
+                    {
+                        _code3Units.Add(_policeBackupUnit);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Game.LogTrivial(e.ToString());
+                }
+            }
+
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+
+                }
+            });
         }
 
         public override void Update()
         {
-            if (Game.IsKeyDown(System.Windows.Forms.Keys.B))
+            GameFiber.StartNew(delegate
             {
-                Game.DisplayNotification("Update (Pressing B)");
-            }
+                _menuPool.ProcessMenus();
+
+                if (Game.IsKeyDown(s_menuKey))
+                {
+                    _menu.Visible = true;
+                }
+            });
         }
 
         public override void Finally()
---
+++ LSPDFR_Reborn/LSPDFR_Reborn/PoliceBackup/PoliceBackupData.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace LSPDFR_Reborn.PoliceBackup
+{
+    [XmlRoot(ElementName = "Vehicle")]
+    public class Vehicle
+    {
+        [XmlAttribute(AttributeName = "color")]
+        public string Color { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Vehicles")]
+    public class Vehicles
+    {
+        [XmlElement(ElementName = "Vehicle")]
+        public List<Vehicle> Vehicle { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Ped")]
+    public class Ped
+    {
+        [XmlAttribute(AttributeName = "chance")]
+        public string Chance { get; set; }
+        [XmlAttribute(AttributeName = "comp_beard")]
+        public string Comp_beard { get; set; }
+        [XmlAttribute(AttributeName = "comp_shirt")]
+        public string Comp_shirt { get; set; }
+        [XmlAttribute(AttributeName = "tex_shit")]
+        public string Tex_shit { get; set; }
+        [XmlAttribute(AttributeName = "comp_pants")]
+        public string Comp_pants { get; set; }
+        [XmlAttribute(AttributeName = "tex_pants")]
+        public string Tex_pants { get; set; }
+        [XmlAttribute(AttributeName = "comp_hands")]
+        public string Comp_hands { get; set; }
+        [XmlAttribute(AttributeName = "comp_shoes")]
+        public string Comp_shoes { get; set; }
+        [XmlAttribute(AttributeName = "comp_eyes")]
+        public string Comp_eyes { get; set; }
+        [XmlAttribute(AttributeName = "comp_accessories")]
+        public string Comp_accessories { get; set; }
+        [XmlAttribute(AttributeName = "tex_accessories")]
+        public string Tex_accessories { get; set; }
+        [XmlAttribute(AttributeName = "comp_tasks")]
+        public string Comp_tasks { get; set; }
+        [XmlAttribute(AttributeName = "tex_tasks")]
+        public string Tex_tasks { get; set; }
+        [XmlAttribute(AttributeName = "comp_shirtoverlay")]
+        public string Comp_shirtoverlay { get; set; }
+        [XmlAttribute(AttributeName = "tex_shirtoverlay")]
+        public string Tex_shirtoverlay { get; set; }
+        [XmlAttribute(AttributeName = "prop_hats")]
+        public string Prop_hats { get; set; }
+        [XmlAttribute(AttributeName = "tex_hats")]
+        public string Tex_hats { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+        [XmlAttribute(AttributeName = "tex_shirt")]
+        public string Tex_shirt { get; set; }
+        [XmlAttribute(AttributeName = "tex_shoes")]
+        public string Tex_shoes { get; set; }
+        [XmlAttribute(AttributeName = "tex_hands")]
+        public string Tex_hands { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Peds")]
+    public class Peds
+    {
+        [XmlElement(ElementName = "Ped")]
+        public List<Ped> Ped { get; set; }
+    }
+
+    [XmlRoot(ElementName = "NonLethals")]
+    public class NonLethals
+    {
+        [XmlElement(ElementName = "NonLethal")]
+        public List<string> NonLethal { get; set; }
+    }
+
+    [XmlRoot(ElementName = "HandGun")]
+    public class HandGun
+    {
+        [XmlAttribute(AttributeName = "chance")]
+        public string Chance { get; set; }
+        [XmlAttribute(AttributeName = "comp_1")]
+        public string Comp_1 { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "HandGuns")]
+    public class HandGuns
+    {
+        [XmlElement(ElementName = "HandGun")]
+        public HandGun HandGun { get; set; }
+    }
+
+    [XmlRoot(ElementName = "LongGun")]
+    public class LongGun
+    {
+        [XmlAttribute(AttributeName = "chance")]
+        public string Chance { get; set; }
+        [XmlAttribute(AttributeName = "comp_1")]
+        public string Comp_1 { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "LongGuns")]
+    public class LongGuns
+    {
+        [XmlElement(ElementName = "LongGun")]
+        public List<LongGun> LongGun { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Weapons")]
+    public class Weapons
+    {
+        [XmlElement(ElementName = "NonLethals")]
+        public NonLethals NonLethals { get; set; }
+        [XmlElement(ElementName = "HandGuns")]
+        public HandGuns HandGuns { get; set; }
+        [XmlElement(ElementName = "LongGuns")]
+        public LongGuns LongGuns { get; set; }
+    }
+
+    [XmlRoot(ElementName = "VehicleSet")]
+    public class VehicleSet
+    {
+        [XmlElement(ElementName = "Vehicles")]
+        public Vehicles Vehicles { get; set; }
+        [XmlElement(ElementName = "Peds")]
+        public Peds Peds { get; set; }
+        [XmlElement(ElementName = "Weapons")]
+        public Weapons Weapons { get; set; }
+        [XmlAttribute(AttributeName = "spawn_distance")]
+        public string Spawn_distance { get; set; }
+    }
+
+    [XmlRoot(ElementName = "LSCity")]
+    public class LSCity
+    {
+        [XmlElement(ElementName = "VehicleSet")]
+        public VehicleSet VehicleSet { get; set; }
+    }
+
+    [XmlRoot(ElementName = "LSCounty")]
+    public class LSCounty
+    {
+        [XmlElement(ElementName = "VehicleSet")]
+        public VehicleSet VehicleSet { get; set; }
+    }
+
+    [XmlRoot(ElementName = "BCounty")]
+    public class BCounty
+    {
+        [XmlElement(ElementName = "VehicleSet")]
+        public VehicleSet VehicleSet { get; set; }
+    }
+
+    [XmlRoot(ElementName = "NY")]
+    public class NY
+    {
+        [XmlElement(ElementName = "VehicleSet")]
+        public VehicleSet VehicleSet { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CP")]
+    public class CP
+    {
+        [XmlElement(ElementName = "VehicleSet")]
+        public VehicleSet VehicleSet { get; set; }
+    }
+
+    [XmlRoot(ElementName = "PoliceBackupUnit")]
+    public class PoliceBackupUnit
+    {
+        [XmlElement(ElementName = "LSCity")]
+        public LSCity LSCity { get; set; }
+        [XmlElement(ElementName = "LSCounty")]
+        public LSCounty LSCounty { get; set; }
+        [XmlElement(ElementName = "BCounty")]
+        public BCounty BCounty { get; set; }
+        [XmlElement(ElementName = "NY")]
+        public NY NY { get; set; }
+        [XmlElement(ElementName = "CP")]
+        public CP CP { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+        [XmlAttribute(AttributeName = "code2")]
+        public string Code2 { get; set; }
+        [XmlAttribute(AttributeName = "code3")]
+        public string Code3 { get; set; }
+        [XmlAttribute(AttributeName = "pursuit")]
+        public string Pursuit { get; set; }
+        [XmlAttribute(AttributeName = "traffic_stop")]
+        public string Traffic_stop { get; set; }
+        [XmlAttribute(AttributeName = "felony_stop")]
+        public string Felony_stop { get; set; }
+    }
+
+    [XmlRoot(ElementName = "PoliceBackupUnits")]
+    public class PoliceBackupUnits
+    {
+        [XmlElement(ElementName = "PoliceBackupUnit")]
+        public List<PoliceBackupUnit> PoliceBackupUnit { get; set; }
+    }
+}
